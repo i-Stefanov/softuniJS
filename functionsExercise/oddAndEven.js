64b25452f7e478c1a08020538b9e33816888b63c
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-02-10 12:32:48 +0200

    Odd and even sum solution

---
+++ functionsExercise/oddAndEven.js
@@ -0,0 +1,14 @@
+function oddAndEvenSum(num) {
+  let numToString = num.toString();
+  let oddSum = 0;
+  let evenSum = 0;
+  for (let i = 0; i < numToString.length; i++) {
+    let element = Number(numToString[i]);
+    if (element % 2 === 0) {
+      evenSum += element;
+    } else {
+      oddSum += element;
+    }
+  }
+  return [oddSum, evenSum];
+}
