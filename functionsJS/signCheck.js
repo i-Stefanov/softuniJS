6eded46eb2e21e4ea4ba25232bc4d79c4a11386f
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-02-10 09:48:39 +0200

    Sign check solution

---
+++ functionsJS/signCheck.js
@@ -0,0 +1,21 @@
+function signCheck(numOne, numTwo, numThree) {
+  let sign = `Positive`;
+  let isNum1Negative = numOne < 0;
+  sign = changeSign(sign, isNum1Negative);
+  let isNum2Negative = numTwo < 0;
+  sign = changeSign(sign, isNum2Negative);
+  let isNum3Negative = numThree < 0;
+  sign = changeSign(sign, isNum3Negative);
+
+  function changeSign(sign, shouldChange) {
+    if (shouldChange === false) {
+      return sign;
+    }
+    if (sign === `Positive`) {
+      sign = `Negative`;
+    } else {
+      sign = `Positive`;
+    }
+    return sign;
+  }
+}
