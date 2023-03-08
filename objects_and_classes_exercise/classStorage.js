2743848a8a3d15081c7e032cbb3af2f0aa3daa1c
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-03-08 16:40:28 +0200

    Solution to class storage problem

--- objects_and_classes_exercise/classStorage.js
+++ objects_and_classes_exercise/classStorage.js
@@ -3,4 +3,20 @@ class Storage {
     this.capacity = capacity;
     this.storage = [];
   }
+  get totalCost() {
+    return this.storage.reduce((acc, el) => {
+      return acc + el.price * el.quantity;
+    }, 0);
+  }
+  addProduct(product) {
+    this.storage.push(product);
+    this.capacity -= product.quantity;
+  }
+  getProducts() {
+    let output = [];
+    this.storage.forEach((product) => {
+      output.push(JSON.stringify(product));
+    });
+    return output.join(`\n`);
+  }
 }
