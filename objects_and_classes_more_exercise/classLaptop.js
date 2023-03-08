e62576763437ed7167b940985f9ca17d57921e14
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-03-08 16:41:17 +0200

    solution to class Laptop

---
+++ objects_and_classes_more_exercise/classLaptop.js
@@ -0,0 +1,33 @@
+class Laptop {
+  constructor(info, quality) {
+    this.info = info;
+    this.quality = quality;
+    this.isOn = false;
+    this.info.producer = info.producer;
+    this.info.age = info.age;
+    this.info.brand = info.brand;
+  }
+  get price() {
+    return 800 - this.info.age * 2 + this.quality * 0.5;
+  }
+  turnOn() {
+    this.isOn = true;
+    this.quality--;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.quality--;
+  }
+  showInfo() {
+    return JSON.stringify(this.info);
+  }
+}
+let info = { producer: "Dell", age: 2, brand: "XPS" };
+let laptop = new Laptop(info, 10);
+laptop.turnOn();
+console.log(laptop.showInfo());
+laptop.turnOff();
+console.log(laptop.quality);
+laptop.turnOn();
+console.log(laptop.isOn);
+console.log(laptop.price);
