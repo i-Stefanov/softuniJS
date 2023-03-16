3c30d37dfacad4ab937cbe2d123dc92886e06e85
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-03-16 11:35:06 +0200

    Example for object sorting

---
+++ associative-arrays/sort-assoc-array.js
@@ -0,0 +1,18 @@
+//declare associative array
+let phoneBook = {
+  Tim: `0834212324`,
+  Joe: `0834212214`,
+  George: `0834212094`,
+  Ivory: `0834212984`,
+};
+// convert the associative array to an array
+let phoneBookentries = Object.entries(phoneBook);
+
+//sort the array
+let sortedentEnries = phoneBookentries.sort((keyValuePairA, keyValuePairB) =>
+  keyValuePairA[0].localeCompare(keyValuePairB[0])
+);
+//convert the array to an object
+let sortedPhoneBook = Object.fromEntries(sortedentEnries);
+
+console.log(sortedPhoneBook);
