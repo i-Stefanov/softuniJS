38671dd765ea949f8651feae71bfceff545e6abe
i-Stefanov <iliqni6e@gmail.com>
2023-04-21 15:22:24 +0300

    simple solutions

---
+++ softuni-JS-Advanced/2-arrays-and-nested-arrays/11-equal-neightbors.js
@@ -0,0 +1,14 @@
+function equalNeighbors(arr) {
+  let neighbors = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let row = arr[i];
+    for (let j = 0; j < row.length; j++) {}
+    console.log(neighbors);
+  }
+}
+equalNeighbors([
+  ["2", "3", "4", "7", "0"],
+  ["4", "0", "5", "3", "4"],
+  ["2", "3", "5", "4", "2"],
+  ["9", "8", "7", "5", "4"],
+]);
