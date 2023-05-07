5370346b30ab0d61337b09300802a1082a78e20c
i-Stefanov <iliqni6e@gmail.com>
2023-05-07 20:55:09 +0300

    solutions

---
+++ softuni-JS-Advanced/13-unit-testing/1-sub-sum.js
@@ -0,0 +1,13 @@
+function subSum(numbers, start, end) {
+  if (!Array.isArray(numbers)) {
+    return NaN;
+  }
+  let startIndex = Math.max(start, 0);
+  let endIndex = Math.min(end, numbers.length - 1);
+  let subNumbers = numbers.slice(startIndex, endIndex + 1);
+  let sum = subNumbers.reduce((a, num) => a + Number(num), 0);
+  console.log(sum);
+  return sum;
+}
+
+subSum([10, 20, 30, 40, 50, 60], 3, 300);
