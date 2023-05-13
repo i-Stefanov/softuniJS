75a9511282a6d00bc112073243dde9e645fca760
i-Stefanov <iliqni6e@gmail.com>
2023-05-13 15:01:28 +0300

    solutions

--- softuni-JS-Advanced/4.1.DOMintro-exercise/01. Subtraction/subtract.js
+++ softuni-JS-Advanced/4.1.DOMintro-exercise/01. Subtraction/subtract.js
@@ -3,10 +3,6 @@ function subtract() {
   let num1 = document.querySelector(`#firstNumber`).value;
   let num2 = document.querySelector(`#secondNumber`).value;
   let sum = parseFloat(num1) - parseFloat(num2);
-  console.log(result);
-  //   console.log(num1);
-  //   console.log(num2);
-  //   console.log(num1.textContent);
-  //   console.log(num2.textContent);
+
   result.textContent = sum;
 }
--- softuni-JS-Advanced/4.1.DOMintro-exercise/02. Pascal or Camel Case/solution.js
+++ softuni-JS-Advanced/4.1.DOMintro-exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  let word = document.querySelector(`#text`).value;
+  const convention = document.querySelector(`#naming-convention`).value;
+  const result = document.querySelector(`#result`);
+  word = word.toLowerCase();
+  const words = word.split(` `);
+  let wordToPrint = ``;
+  if (convention === `Pascal Case`) {
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+  } else if (convention === `Camel Case`) {
+    for (let i = 1; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+  } else {
+    result.textContent = `Error!`;
+    return;
+  }
+  wordToPrint = words.join(``);
+  result.textContent = wordToPrint;
 }
