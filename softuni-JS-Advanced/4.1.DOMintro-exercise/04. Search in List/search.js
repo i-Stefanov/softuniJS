3c6cc7388ae1565ad61016cf33cb7e6e1b8349b1
i-Stefanov <iliqni6e@gmail.com>
2023-05-13 16:09:01 +0300

    solutions

--- softuni-JS-Advanced/4.1.DOMintro-exercise/04. Search in List/search.js
+++ softuni-JS-Advanced/4.1.DOMintro-exercise/04. Search in List/search.js
@@ -1,3 +1,28 @@
 function search() {
-   // TODO
+  const townsEl = document.querySelectorAll(`#towns li:nth-child(n)`);
+  let searchText = document.querySelector(`#searchText`);
+  const result = document.querySelector(`#result`);
+  let matches = 0;
+  let towns = Array.from(townsEl);
+  towns.forEach((town) => {
+    if (
+      town.style.textDecoration !== `` &&
+      town.style.textDecoration === `underline`
+    ) {
+      town.style.textDecoration = `none`;
+      town.style.fontWeight = `normal`;
+    }
+    if (
+      town.textContent.includes(searchText.value) &&
+      searchText.value !== ``
+    ) {
+      town.style.textDecoration = `underline`;
+      town.style.fontWeight = `bold`;
+      matches++;
+    }
+  });
+
+  result.textContent = `${matches} matches found`;
+  searchText.value = ``;
+  matches = 0;
 }
