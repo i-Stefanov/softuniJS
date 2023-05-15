cb3a9e002022e108d6a732244e4e46462fe526c4
i-Stefanov <iliqni6e@gmail.com>
2023-05-15 12:50:46 +0300

    solutions

--- softuni-JS-Advanced/4.1.DOMintro-exercise/06. Format the Text/solution.js
+++ softuni-JS-Advanced/4.1.DOMintro-exercise/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  const input = document.querySelector(`#input`).value;
+  let output = document.querySelector(`#output`);
+  output.innerHTML = ``;
+  let sentences = input.split(`.`).filter((s) => s !== "");
+
+  // console.log(input);
+  // console.log(sentences);
+  while (sentences.length > 0) {
+    let sentencesToPutInParagraph = sentences.splice(0, 3);
+    output.innerHTML += `<p>${sentencesToPutInParagraph.join(`.`)}.</p>`;
+  }
 }
