67f91e91afed9bc575fb808eb5055cdffa27e141
i-Stefanov <iliqni6e@gmail.com>
2023-05-15 09:35:43 +0300

    solutions

--- softuni-JS-Advanced/4.1.DOMintro-exercise/05. Table - Search Engine/solution.js
+++ softuni-JS-Advanced/4.1.DOMintro-exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,24 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    const tableCellsEl = document.querySelectorAll(`td:not([colspan])`);
+    let tableCells = Array.from(tableCellsEl);
+    const searchField = document.querySelector(`#searchField`);
+    console.log(tableCells);
+    tableCells.forEach((cell) => {
+      cell.parentElement.classList.remove(`select`);
+    });
+    tableCells.forEach((cell) => {
+      if (
+        cell.textContent
+          .toLowerCase()
+          .includes(searchField.value.toLowerCase()) &&
+        searchField.value != ``
+      ) {
+        cell.parentElement.classList.add(`select`);
+      }
+    });
+    searchField.value = ``;
+  }
 }
