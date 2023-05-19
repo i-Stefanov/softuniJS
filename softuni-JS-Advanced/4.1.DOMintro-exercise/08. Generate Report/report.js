1992aa93512d6ef08cc80841b0cb3d7b082ecef4
i-Stefanov <iliqni6e@gmail.com>
2023-05-19 15:14:11 +0300

    initial commit for this task

--- softuni-JS-Advanced/4.1.DOMintro-exercise/08. Generate Report/report.js
+++ softuni-JS-Advanced/4.1.DOMintro-exercise/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+  const employeesDataChecBox = document.querySelectorAll(`th>input`);
+  const employees = Array.from(document.querySelectorAll(`tr td:nth-child(1)`));
+  const department = Array.from(
+    document.querySelectorAll(`tr td:nth-child(2)`)
+  );
+  const status = Array.from(document.querySelectorAll(`tr td:nth-child(3)`));
+  const dateHired = Array.from(document.querySelectorAll(`tr td:nth-child(4)`));
+  const benefits = Array.from(document.querySelectorAll(`tr td:nth-child(5)`));
+  const compensation = Array.from(
+    document.querySelectorAll(`tr td:nth-child(6)`)
+  );
+  employeesDataChecBox.forEach((checkBox) => {
+    if (checkBox.checked) {
+      console.log(checkBox["name"]);
+    }
+  });
+  const reting = Array.from(document.querySelectorAll(`tr td:nth-child(7)`));
+  let employeesArr = Array.from(employees);
+  //   arr.forEach((e) => {
+  //     console.log(e.textContent);
+  //   });
 }
