2eb6b038b7908e0b72899e438c8083cf4a253bcc
i-Stefanov <iliqni6e@gmail.com>
2023-05-24 17:22:08 +0300

    workig solution

--- softuni-JS-Advanced/11.DOM-manipulation/06. Dynamic-Validation/app.js
+++ softuni-JS-Advanced/11.DOM-manipulation/06. Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    console.log('TODO:...');
+  const emailElement = document.querySelector(`#email`);
+  const pattern = /[a-z]+@[a-z]+\.[a-z]+/gm;
+  emailElement.addEventListener(`change`, () => {
+    if (pattern.test(emailElement.value)) {
+      emailElement.classList.remove(`error`);
+    } else {
+      emailElement.classList.add(`error`);
+    }
+  });
 }
