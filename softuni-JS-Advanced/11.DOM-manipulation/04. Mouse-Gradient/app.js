dab7f283fef5aabd3103c94372c2319bb839c0d3
i-Stefanov <iliqni6e@gmail.com>
2023-05-24 16:22:13 +0300

    workig solution

--- softuni-JS-Advanced/11.DOM-manipulation/04. Mouse-Gradient/app.js
+++ softuni-JS-Advanced/11.DOM-manipulation/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const gradientBox = document.querySelector(`#gradient`);
+  let result = document.querySelector(`#result`);
+
+  gradientBox.addEventListener(`mousemove`, (e) => {
+    let x = e.offsetX;
+    let size = e.target.clientWidth;
+    let percentage = Math.floor((x / size) * 100);
+    result.textContent = `${percentage}%`;
+  });
 }
