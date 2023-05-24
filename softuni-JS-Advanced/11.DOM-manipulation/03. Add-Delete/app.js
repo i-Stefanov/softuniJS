cf5aeddab2d2a9682263fa0ad3f4b6e20394af05
i-Stefanov <iliqni6e@gmail.com>
2023-05-24 15:45:09 +0300

    workig solution

--- softuni-JS-Advanced/11.DOM-manipulation/03. Add-Delete/app.js
+++ softuni-JS-Advanced/11.DOM-manipulation/03. Add-Delete/app.js
@@ -1,3 +1,15 @@
 function addItem() {
-    //TODO...
+  const ul = document.querySelector(`#items`);
+  const input = document.querySelector(`#newItemText`);
+  let li = document.createElement(`li`);
+  let deleteButton = document.createElement(`a`);
+  deleteButton.href = `#`;
+  deleteButton.textContent = `[Delete]`;
+  li.textContent = input.value;
+  li.appendChild(deleteButton);
+  ul.appendChild(li);
+  deleteButton.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    li.remove();
+  });
 }
