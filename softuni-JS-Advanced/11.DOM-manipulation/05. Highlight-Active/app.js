412868d6993d1809596dc9b62dd30f4cb921d827
i-Stefanov <iliqni6e@gmail.com>
2023-05-24 17:00:47 +0300

    workig solution

--- softuni-JS-Advanced/11.DOM-manipulation/05. Highlight-Active/app.js
+++ softuni-JS-Advanced/11.DOM-manipulation/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+  const inputs = Array.from(document.querySelectorAll(`input`));
+  const divs = Array.from(document.querySelectorAll(`div`));
+  inputs.forEach((input, i) => {
+    input.addEventListener(`focus`, () => {
+      inputs[i].parentNode.classList.add(`focused`);
+    });
+  });
+  inputs.forEach((input, i) => {
+    input.addEventListener(`blur`, () => {
+      inputs[i].parentNode.classList.remove(`focused`);
+    });
+  });
 }
