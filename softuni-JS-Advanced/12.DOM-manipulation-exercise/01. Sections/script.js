2b73b505a6e4d3eb621b0ec630539f5fe62b8407
i-Stefanov <iliqni6e@gmail.com>
2023-05-24 18:58:07 +0300

    solution

--- softuni-JS-Advanced/12.DOM-manipulation-exercise/01. Sections/script.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+  const mainDiv = document.querySelector(`#content`);
+  let div;
+  let p;
+  words.forEach((word) => {
+    div = document.createElement(`div`);
+    p = document.createElement(`p`);
+    p.style.display = `none`;
+    p.textContent = word;
+    div.appendChild(p);
+    mainDiv.appendChild(div);
+  });
+  let divs = Array.from(document.querySelectorAll(`div:not(#content)`));
+  let pElements = Array.from(document.querySelectorAll(`p`));
+  divs.forEach((div, i) => {
+    div.addEventListener(`click`, () => {
+      console.log(`clicked`);
+      pElements[i].style.display = `inline`;
+    });
+  });
 }
