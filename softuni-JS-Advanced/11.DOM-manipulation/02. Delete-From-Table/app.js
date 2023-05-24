729697cc7fe9f7e950936a473546af2b194058af
i-Stefanov <iliqni6e@gmail.com>
2023-05-24 15:28:29 +0300

    modified .gitignore,solutions

--- softuni-JS-Advanced/11.DOM-manipulation/02. Delete-From-Table/app.js
+++ softuni-JS-Advanced/11.DOM-manipulation/02. Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const emails = document.querySelectorAll(`td:nth-child(2)`);
+  const input = document.querySelector(`[name="email"]`);
+  let result = document.querySelector(`#result`);
+  Array.from(emails).forEach((email) => {
+    if (input.value === email.textContent) {
+      email.parentNode.remove();
+      result.textContent = `Deleted.`;
+    } else {
+      result.textContent = `Not found.`;
+    }
+  });
+
+  input.value = ``;
 }
