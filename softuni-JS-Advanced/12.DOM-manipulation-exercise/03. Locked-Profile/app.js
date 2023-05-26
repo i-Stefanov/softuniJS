eac39938956ef7ecd83de7635238adae87f9b29e
i-Stefanov <iliqni6e@gmail.com>
2023-05-26 20:24:58 +0300

    solution

--- softuni-JS-Advanced/12.DOM-manipulation-exercise/03. Locked-Profile/app.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/03. Locked-Profile/app.js
@@ -6,14 +6,14 @@ function lockedProfile() {
   function onClick(e) {
     let unlock = e.target.parentElement.querySelector(`input[value="unlock"]`);
     let hiddenInfo = e.target.parentElement.querySelector(`div`);
-    let button = e.target;
+
     if (unlock.checked) {
       hiddenInfo.style.display === `block`
         ? (hiddenInfo.style.display = `none`)
         : (hiddenInfo.style.display = `block`);
-      button.textContent === `Show more`
-        ? (button.textContent = `Hide it`)
-        : (button.textContent = `Show more`);
+      e.target.textContent === `Show more`
+        ? (e.target.textContent = `Hide it`)
+        : (e.target.textContent = `Show more`);
     }
   }
 }
