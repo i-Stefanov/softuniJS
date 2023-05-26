59e681b1c560e5112263c5008fec37c2f22b83ec
i-Stefanov <iliqni6e@gmail.com>
2023-05-26 19:31:36 +0300

    solution

--- softuni-JS-Advanced/12.DOM-manipulation-exercise/02. Time-Converter/solution.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/02. Time-Converter/solution.js
@@ -1,4 +1,33 @@
 function attachEventsListeners() {
-
-    console.log('TODO:...');
+  const daysInput = document.querySelector(`#days`);
+  const hoursInput = document.querySelector(`#hours`);
+  const minutesInput = document.querySelector(`#minutes`);
+  const secondsInput = document.querySelector(`#seconds`);
+  const buttons = Array.from(document.querySelectorAll(`input[type="button"]`));
+  const ratios = {
+    days: 1,
+    hours: 24,
+    minutes: 1440,
+    seconds: 86400,
+  };
+  function convert(value, unit) {
+    let unitsPerUnit = value / ratios[unit];
+    return {
+      days: unitsPerUnit * ratios.days,
+      hours: unitsPerUnit * ratios.hours,
+      minutes: unitsPerUnit * ratios.minutes,
+      seconds: unitsPerUnit * ratios.seconds,
+    };
+  }
+  buttons.forEach((button) => {
+    button.addEventListener(`click`, onConvert);
+  });
+  function onConvert(event) {
+    let input = event.target.parentElement.querySelector(`input[type="text"]`);
+    let time = convert(Number(input.value), input.id);
+    daysInput.value = time.days;
+    hoursInput.value = time.hours;
+    minutesInput.value = time.minutes;
+    secondsInput.value = time.seconds;
+  }
 }
