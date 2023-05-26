81a455cb8774b1dc14028144df741c6ed6dd7b69
i-Stefanov <iliqni6e@gmail.com>
2023-05-26 20:32:25 +0300

    solution

--- softuni-JS-Advanced/12.DOM-manipulation-exercise/04. Fill-Dropdown/dropdown.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+  const newItemText = document.querySelector(`#newItemText`);
+  const newItemValue = document.querySelector(`#newItemValue`);
+  const menu = document.querySelector(`#menu`);
+  const option = document.createElement(`option`);
+  option.textContent = newItemText.value;
+  option.value = newItemValue.value;
+  menu.appendChild(option);
+  newItemText.value = ``;
+  newItemValue.value = ``;
 }
