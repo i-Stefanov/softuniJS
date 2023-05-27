7a732c11427b5aa00726416339f5ec736f2f4726
i-Stefanov <iliqni6e@gmail.com>
2023-05-27 22:23:56 +0300

    partial solution

--- softuni-JS-Advanced/12.DOM-manipulation-exercise/05. Encode-and-Decode-Messages/app.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,31 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const [messageToEncode, codedMessage] = Array.from(
+    document.querySelectorAll(`textarea`)
+  );
+  const [encodeBtn, decodeBtn] = Array.from(
+    document.querySelectorAll(`button`)
+  );
+
+  encodeBtn.addEventListener(`click`, () => {
+    let stringToConvert = messageToEncode.value;
+    let newString = ``;
+    let currentChar = ``;
+    for (let i = 0; i < stringToConvert.length; i++) {
+      currentChar = stringToConvert[i];
+      newString += String.fromCharCode(stringToConvert.charCodeAt(i) + 1);
+    }
+    codedMessage.value = newString;
+    messageToEncode.value = ``;
+  });
+  decodeBtn.addEventListener(`click`, () => {
+    let stringToConvert = codedMessage.value;
+    let newString = ``;
+    let currentChar = ``;
+    for (let i = 0; i < stringToConvert.length; i++) {
+      currentChar = stringToConvert[i];
+      newString += String.fromCharCode(stringToConvert.charCodeAt(i) - 1);
+    }
+    codedMessage.value = ``;
+    codedMessage.value = newString;
+  });
 }
--- softuni-JS-Advanced/12.DOM-manipulation-exercise/06. Furniture/solution.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/06. Furniture/solution.js
@@ -1,4 +1,46 @@
 function solve() {
-
-  //TODO...
+  const [items, cart] = Array.from(document.querySelectorAll(`textarea`));
+  const [generateBtn, buyBtn] = Array.from(document.querySelectorAll(`button`));
+  const tbody = document.querySelector(`tbody`);
+  let tr = document.createElement(`tr`);
+  let tdImg = document.createElement(`td`);
+  let tdName = document.createElement(`td`);
+  let tdPrice = document.createElement(`td`);
+  let tdDecFactor = document.createElement(`td`);
+  let tdCheckbox = document.createElement(`td`);
+  let pName = document.createElement(`p`);
+  let pPrice = document.createElement(`p`);
+  let pDecFactor = document.createElement(`p`);
+  let img = document.createElement(`img`);
+  let checkbox = document.createElement(`input`);
+  checkbox.type = `checkbox`;
+  generateBtn.addEventListener(`click`, generate);
+  function generate() {
+    const products = JSON.parse(items.value);
+    products.forEach((product) => {
+      img.src = product.img;
+      pName.textContent = product.name;
+      pPrice.textContent = product.price;
+      pDecFactor.textContent = product.decFactor;
+      tdImg.appendChild(img);
+      tdName.appendChild(pName);
+      tdPrice.appendChild(pPrice);
+      tdDecFactor.appendChild(pDecFactor);
+      tdCheckbox.appendChild(checkbox);
+      tr.appendChild(tdImg);
+      tr.appendChild(tdName);
+      tr.appendChild(tdPrice);
+      tr.appendChild(tdDecFactor);
+      tr.appendChild(tdCheckbox);
+      tbody.appendChild(tr);
+    });
+  }
 }
+// [
+//   {
+//     name: "Sofa",
+//     img: "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg",
+//     price: 150,
+//     decFactor: 1.2,
+//   },
+// ];
