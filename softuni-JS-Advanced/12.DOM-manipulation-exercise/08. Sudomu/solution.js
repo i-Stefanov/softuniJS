4a1c28a1052a31bab48ee6353d872281102df3ac
i-Stefanov <iliqni6e@gmail.com>
2023-06-05 15:54:02 +0300

    working solution

--- softuni-JS-Advanced/12.DOM-manipulation-exercise/08. Sudomu/solution.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/08. Sudomu/solution.js
@@ -1,4 +1,42 @@
 function solve() {
-
-    //TODO...
+  const [quickCheckBtn, clearBtn] = document.querySelectorAll(`button`);
+  const table = document.querySelector(`table`);
+  const inputs = Array.from(document.querySelectorAll(`tbody tr input`));
+  const p = document.querySelector(`p`);
+  quickCheckBtn.addEventListener(`click`, quickCheck);
+  clearBtn.addEventListener(`click`, clear);
+  function quickCheck() {
+    let matrix = [
+      [inputs[0].value, inputs[1].value, inputs[2].value],
+      [inputs[3].value, inputs[4].value, inputs[5].value],
+      [inputs[6].value, inputs[7].value, inputs[8].value],
+    ];
+    let isSudomu = true;
+    for (let i = 0; i < matrix.length; i++) {
+      const row = matrix[i];
+      let col = matrix.map((row) => row[i]);
+      if (
+        row.length !== new Set(row).size ||
+        col.length !== new Set(col).size
+      ) {
+        isSudomu = false;
+      }
+    }
+    if (isSudomu) {
+      table.style.border = "2px solid green";
+      p.textContent = "You solve it! Congratulations!";
+      p.style.color = `green`;
+    } else {
+      table.style.border = "2px solid red";
+      p.textContent = "NOP! You are not done yet...";
+      p.style.color = `red`;
+    }
+  }
+  function clear() {
+    for (const input of inputs) {
+      input.value = ``;
+    }
+    table.style.border = "";
+    p.textContent = "";
+  }
 }
--- softuni-JS-Advanced/12.DOM-manipulation-exercise/09. JavaScript-Quiz/solution.js
+++ softuni-JS-Advanced/12.DOM-manipulation-exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-  //TODO...
+  const answers = Array.from(document.querySelectorAll(`.answer-text`));
+  let correctAnswersCounter = 0;
+  let sections = Array.from(document.querySelectorAll(`section`));
+  let index = 0;
+  const correctAnswers = [
+    `onclick`,
+    `JSON.stringify()`,
+    `A programming API for HTML and XML documents`,
+  ];
+  answers.forEach((answer) => {
+    answer.addEventListener(`click`, answered);
+  });
+  function answered(e) {
+    let answerClicked = e.target.textContent;
+    if (correctAnswers.includes(answerClicked)) {
+      correctAnswersCounter++;
+      console.log(correctAnswersCounter);
+    }
+    console.log(sections[index]);
+    sections[index].style.display = `none`;
+    index++;
+    index !== 3
+      ? (sections[index].style.display = `block`)
+      : displayResult(correctAnswersCounter);
+  }
+  function displayResult(points) {
+    let result = document.querySelector(`#results h1`);
+    document.querySelector(`#results`).style.display = `block`;
+
+    if (points === sections.length) {
+      result.textContent = `You are recognized as top JavaScript fan!`;
+    } else {
+      result.textContent = `You have ${correctAnswersCounter} right answers`;
+    }
+  }
 }
