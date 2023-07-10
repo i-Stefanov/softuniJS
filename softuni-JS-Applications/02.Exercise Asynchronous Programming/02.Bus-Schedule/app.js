316b554de7056730605ea53f6d3da65037a40dc2
i-Stefanov <iliqni6e@gmail.com>
2023-07-10 14:18:34 +0300

    solution

--- softuni-JS-Applications/02.Exercise Asynchronous Programming/02.Bus-Schedule/app.js
+++ softuni-JS-Applications/02.Exercise Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,10 +1,35 @@
 function solve() {
-  function depart() {
-    console.log("Depart TODO...");
+  let stopId = `depot`;
+  let info = document.querySelector(`.info`);
+  let departBtn = document.querySelector(`#depart`);
+  let arriveBtn = document.querySelector(`#arrive`);
+  let arriving = ``;
+  async function depart() {
+    try {
+      let response = await fetch(
+        `http://localhost:3030/jsonstore/bus/schedule/${stopId}`
+      );
+      if (response.status !== 200) {
+        throw new Error();
+      }
+      let data = await response.json();
+      stopId = data.next;
+      info.textContent = `Next stop ${data.name}`;
+      arriving = data.name;
+      departBtn.disabled = true;
+      arriveBtn.disabled = false;
+      console.log(stopId);
+    } catch (error) {
+      info.textContent = "error";
+      arriveBtn.disabled = true;
+      departBtn.disabled = true;
+    }
   }
 
   function arrive() {
-    console.log("Arrive TODO...");
+    info.textContent = `Arriving at ${arriving}`;
+    arriveBtn.disabled = true;
+    departBtn.disabled = false;
   }
 
   return {
