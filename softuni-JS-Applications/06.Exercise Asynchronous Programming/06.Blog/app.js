a4c47c1465598c875adfbae55886fc2393445570
i-Stefanov <iliqni6e@gmail.com>
2023-07-11 15:28:58 +0300

    partial solution

--- softuni-JS-Applications/06.Exercise Asynchronous Programming/06.Blog/app.js
+++ softuni-JS-Applications/06.Exercise Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,62 @@
 function attachEvents() {
-    console.log('TODO...');
+  const btnLoadPosts = document.querySelector(`#btnLoadPosts`);
+  const btnViewPost = document.querySelector(`#btnViewPost`);
+  const titleEl = document.querySelector(`#post-title`);
+  const postBody = document.querySelector(`#post-body`);
+  const postComments = document.querySelector(`#post-comments`);
+  let selectPosts = document.querySelector(`#posts`);
+  selectPosts.innerHTML = "";
+  btnLoadPosts.addEventListener(`click`, loadPosts);
+  btnViewPost.addEventListener(`click`, viewPost);
+  let posts;
+  async function loadPosts() {
+    try {
+      const response = await fetch(
+        `http://localhost:3030/jsonstore/blog/posts`
+      );
+      posts = await response.json();
+      if (!posts) {
+        throw new Error();
+      }
+      Object.entries(posts).forEach(([postId, postBody]) => {
+        const option = document.createElement(`option`);
+        option.textContent = postBody.title.toUpperCase();
+        option.value = postId;
+        selectPosts.appendChild(option);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function viewPost() {
+    try {
+      const response = await fetch(
+        `http://localhost:3030/jsonstore/blog/comments`
+      );
+      const comments = await response.json();
+      if (!comments) {
+        throw new Error();
+      }
+      let currentComments = Object.values(comments).filter(
+        (comment) => comment.postId == selectPosts.value
+      );
+
+      let selectedOption = document.querySelector(
+        `option[value="${selectPosts.value}"]`
+      );
+      console.log(selectedOption.textContent);
+      postBody.textContent = posts[selectPosts.value].body;
+      titleEl.textContent = selectedOption.textContent;
+      postComments.innerHTML = "";
+      currentComments.forEach((comment) => {
+        const li = document.createElement(`li`);
+        li.textContent = comment.text;
+        postComments.appendChild(li);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 attachEvents();
