236915f8c0da3d5be2ed65ebea442a6d51a01adc
i-Stefanov <iliqni6e@gmail.com>
2023-07-13 15:53:53 +0300

    Messanger solution

--- softuni-JS-Applications/08.Remote data and authentication-exercise/01.Messenger/app.js
+++ softuni-JS-Applications/08.Remote data and authentication-exercise/01.Messenger/app.js
@@ -1,5 +1,35 @@
 function attachEvents() {
-    console.log('TODO...');
+  const textarea = document.querySelector(`#messages`);
+  const sendBtn = document.querySelector(`#submit`);
+  const refreshBtn = document.querySelector(`#refresh`);
+  const authorField = document.querySelector(`[name="author"]`);
+  const contentField = document.querySelector(`[name="content"]`);
+  sendBtn.addEventListener(`click`, sendMessage);
+  refreshBtn.addEventListener(`click`, refresh);
+  async function sendMessage() {
+    const author = authorField.value;
+    const content = contentField.value;
+    let messageData = {
+      author,
+      content,
+    };
+    await fetch("http://localhost:3030/jsonstore/messenger", {
+      method: "POST",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messageData),
+    });
+  }
+  async function refresh() {
+    const url = "http://localhost:3030/jsonstore/messenger";
+    const response = await fetch(url);
+    const data = await response.json();
+    const contentToBeDisplayed = Object.values(data).map(
+      ({ author, content }) => `${author}: ${content}`
+    );
+    textarea.textContent = contentToBeDisplayed.join(`\n`);
+  }
 }
 
 attachEvents();
