687541bb1eff26cbf098a949f5612a361c462e7b
i-Stefanov <iliqni6e@gmail.com>
2023-07-17 20:05:27 +0300

    commit

--- softuni-JS-Applications/08.Remote data and authentication-exercise/02.Phonebook/app.js
+++ softuni-JS-Applications/08.Remote data and authentication-exercise/02.Phonebook/app.js
@@ -1,5 +1,27 @@
 function attachEvents() {
-    console.log('TODO...');
+  const loadBtn = document.querySelector(`#btnLoad`);
+  const createBtn = document.querySelector(`#btnCreate`);
+  const phonebookUl = document.querySelector(`#phonebook`);
+  const pesronInput = document.querySelector(`#person`);
+  const phoneInput = document.querySelector(`#phone`);
+  loadBtn.addEventListener(`click`, loadInfo);
+  async function loadInfo() {
+    let url = `http://localhost:3030/jsonstore/phonebook`;
+    const response = await fetch(url);
+    const phonebookData = await response.json();
+    let phonebook = Object.values(phonebookData).map(
+      ({ person, phone }) => `${person}: ${phone}`
+    );
+    console.log(phonebook);
+    phonebook.forEach((record) => {
+      const li = document.createElement(`li`);
+      li.textContent = record;
+      const deleteBtn = document.createElement(`button`);
+      deleteBtn.textContent = `Delete`;
+      li.appendChild(deleteBtn);
+      phonebookUl.appendChild(li);
+    });
+  }
 }
 
 attachEvents();
--- softuni-JS-Applications/08.Remote data and authentication-exercise/04.Book-library/app.js
+++ softuni-JS-Applications/08.Remote data and authentication-exercise/04.Book-library/app.js
@@ -1,1 +1,9 @@
-console.log('My requests...')
+async function createBook() {
+  const url = `http://localhost:3030/jsonstore/collections/books`;
+  const submitBtn = document.querySelector(`form button`);
+  submitBtn.addEventListener(`click`, submitFn);
+  function submitFn(e) {
+    e.preventDefault();
+    console.log(`button clicked`);
+  }
+}
