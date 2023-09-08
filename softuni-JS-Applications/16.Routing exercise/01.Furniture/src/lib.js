8b2f902163f393c5d40f21c7799f98f2524f0d06
i-Stefanov <iliqni6e@gmail.com>
2023-09-08 11:58:33 +0300

    initial commit

---
+++ softuni-JS-Applications/16.Routing exercise/01.Furniture/src/lib.js
@@ -0,0 +1,3 @@
+// file to shorten the paths to the modules
+export { render, html, nothing } from "../node_modules/lit-html/lit-html.js";
+export { default as page } from "../node_modules/page/page.mjs";
