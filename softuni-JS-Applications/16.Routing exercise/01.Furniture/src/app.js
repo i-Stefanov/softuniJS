31b7a5751da10eaaf5c0a66c6867874a8d4c7faa
i-Stefanov <iliqni6e@gmail.com>
2023-09-08 12:57:17 +0300

    Configure routing with placehoder modules

---
+++ softuni-JS-Applications/16.Routing exercise/01.Furniture/src/app.js
@@ -0,0 +1,22 @@
+import { showHome } from "./views/home.js";
+import { page, render } from "./lib.js";
+import { showDetails } from "./views/details.js";
+import { showCreate } from "./views/create.js";
+import { showRegister } from "./views/register.js";
+import { showLogin } from "./views/login.js";
+import { showEdit } from "./views/edit.js";
+import { showMyFurniture } from "./views/my-furniture.js";
+const root = document.querySelector("div.container");
+page(decorateContext);
+page("/", showHome);
+page("/details", showDetails);
+page("/create", showCreate);
+page("/register", showRegister);
+page("/login", showLogin);
+page("/edit/:id", showEdit);
+page("/my-furniture", showMyFurniture);
+page.start();
+function decorateContext(ctx, next) {
+  ctx.render = (content) => render(content, root);
+  next();
+}
--- softuni-JS-Applications/16.Routing exercise/01.Furniture/src/lib.js
+++ softuni-JS-Applications/16.Routing exercise/01.Furniture/src/lib.js
@@ -1,3 +1,4 @@
-// file to shorten the paths to the modules
-export { render, html, nothing } from "../node_modules/lit-html/lit-html.js";
-export { default as page } from "../node_modules/page/page.mjs";
+import { render, html, nothing } from "../node_modules/lit-html/lit-html.js";
+import page from "../node_modules/page/page.mjs";
+import { until } from "../node_modules/lit-html/directives/until.js";
+export { render, html, nothing, page, until };
