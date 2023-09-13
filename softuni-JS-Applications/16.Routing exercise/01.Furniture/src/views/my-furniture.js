d55216e89fd20d7d60601c5f4ba313f845bd9ec3
i-Stefanov <iliqni6e@gmail.com>
2023-09-13 16:21:28 +0300

    my furniture shows only the items added by the current user

--- softuni-JS-Applications/16.Routing exercise/01.Furniture/src/views/my-furniture.js
+++ softuni-JS-Applications/16.Routing exercise/01.Furniture/src/views/my-furniture.js
@@ -1,3 +1,38 @@
-export function showMyFurniture() {
-  console.log("my-furniture page");
+import { getMyItems } from "../api/data.js";
+import { html, until } from "../lib.js";
+import { getUserData } from "../util.js";
+// dataPromise = loadItems()
+const myFurnitureTemplate = (dataPromise) => html`
+  <div class="row space-top">
+    <div class="col-md-12">
+      <h1>My furniture</h1>
+      <p>This is a list of your publications</p>
+    </div>
+  </div>
+  <div class="row space-top">
+    ${until(dataPromise, html`<p>Loading &hellip;</p>`)}
+  </div>
+`;
+const itemTemplate = (item) => html`<div class="col-md-4">
+  <div class="card text-white bg-primary">
+    <div class="card-body">
+      <img src=${item.img} />
+      <p>${item.description}</p>
+      <footer>
+        <p>Price: <span>${item.price} $</span></p>
+      </footer>
+      <div>
+        <a href=${`/details/${item._id}`} class="btn btn-info">Details</a>
+      </div>
+    </div>
+  </div>
+</div>`;
+export function showMyFurniture(ctx) {
+  const userId = getUserData().id;
+  ctx.render(myFurnitureTemplate(loadItems(userId)));
+}
+
+async function loadItems(userId) {
+  const items = await getMyItems(userId);
+  return items.map(itemTemplate);
 }
