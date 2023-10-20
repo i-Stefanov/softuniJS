afbccf5b3e739afefa81fd444f05889f90a4f28a
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-21 00:30:06 +0300

    scaffold

--- softuni-JS-WEB/EXAM-PREP-PETSTAGRAM-SKELETON/src/routes.js
+++ softuni-JS-WEB/EXAM-PREP-PETSTAGRAM-SKELETON/src/routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 // TODO add controller routes
-router.get("/", (req, res) => {
-  res.render("home");
+const homeController = require("./controllers/homeController");
+const userController = require("./controllers/userController");
+router.use(homeController);
+router.use("/users", userController);
+router.get("*", (req, res) => {
+  res.redirect("/404");
 });
+
 module.exports = router;
