9f5711e049b0e63551d1236f7a7d4d3af2d04a33
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-30 14:56:48 +0200

    add some components

--- ReactJS/04.React Components Workshop/user-list-workshop/client/src/App.jsx
+++ ReactJS/04.React Components Workshop/user-list-workshop/client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import "./App.css";
+import UserList from "./components/UserList";
+import * as userService from "./services/userService";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    userService
+      .getAll()
+      .then((usersList) => {
+        // actualize the users array with all the users returned from the getAll() function
+        setUsers(usersList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []); // the empty array means that the function will be executed every time the app reloads
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+          <UserList />
+        </section>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
