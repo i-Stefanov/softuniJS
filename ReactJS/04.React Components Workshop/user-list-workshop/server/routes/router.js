48fc1c08fd760283167065826dc9b7fa18e83c84
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-30 14:55:24 +0200

    add cors

--- ReactJS/04.React Components Workshop/user-list-workshop/server/routes/router.js
+++ ReactJS/04.React Components Workshop/user-list-workshop/server/routes/router.js
@@ -1,5 +1,7 @@
-const userRouter = require('./user.js');
+const userRouter = require("./user.js");
+const cors = require("cors");
 
 module.exports = (app) => {
-  app.use('/api/users', userRouter);
+  app.use(cors());
+  app.use("/api/users", userRouter);
 };
