d2575924d3aa23c0b7de98753eabb34a7acdf5b3
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-30 14:55:53 +0200

    get data from the server

---
+++ ReactJS/04.React Components Workshop/user-list-workshop/client/src/services/userService.js
@@ -0,0 +1,6 @@
+const baseUrl = "http://localhost:3005/api/users";
+export const getAll = async () => {
+  const response = await fetch(baseUrl);
+  const result = await response.json();
+  return result.users;
+};
