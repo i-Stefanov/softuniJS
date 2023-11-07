91b362aab2492142c3414294c57a3cafc699e14a
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-11-07 18:57:32 +0200

    create file architecture

--- ReactJS/07.Routing-workshop/client/src/App.jsx
+++ ReactJS/07.Routing-workshop/client/src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Catalog from "./components/Catalog/Catalog";
+import CreateGame from "./components/CreateGame/CreateGame";
+import Register from "./components/Register/Register";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>React App!!!</h1>
+      <div id="box">
+        <Header />
+        {/* <!-- Main Content --> */}
+        <main id="main-content">
+          <Home />
+          {/* <CreateGame></CreateGame> */}
+          {/* <Register></Register> */}
+          {/* <Catalog></Catalog> */}
+          {/* <Login /> */}
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
