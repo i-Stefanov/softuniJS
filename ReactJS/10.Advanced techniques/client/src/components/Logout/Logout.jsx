348b19ecf25a94780eed1327482ce87c00812a6c
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-11-23 20:35:28 +0200

    add useEffect to logout

--- ReactJS/10.Advanced techniques/client/src/components/Logout/Logout.jsx
+++ ReactJS/10.Advanced techniques/client/src/components/Logout/Logout.jsx
@@ -1,10 +1,12 @@
 import { Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Logout() {
   // todo
   const { onLogout } = useContext(AuthContext);
-  onLogout();
+  useEffect(() => {
+    onLogout();
+  }, []);
   return <Navigate to="/" />;
 }
