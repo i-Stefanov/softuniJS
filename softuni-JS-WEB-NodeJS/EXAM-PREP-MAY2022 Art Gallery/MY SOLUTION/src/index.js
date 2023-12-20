54b1fa94291ec99ac08bf702c503b1d847b145dc
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-12-20 12:28:04 +0200

    initial setup

---
+++ softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const hbs = require("express-handlebars");
+
+const routes = require("./routes");
+
+const app = express();
+// DB setup
+mongoose
+  .connect("mongodb://127.0.0.1:27017/art-gallery")
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((err) => console.log("DB error", err.message));
+
+// set template extension
+app.engine("hbs", hbs.engine({ extname: "hbs" }));
+
+app.set("view engine", "hbs");
+
+const PORT = "3000";
+
+// config body parser for form handling (req.body)
+app.use(express.urlencoded({ extended: false }));
+
+// set path to static files
+app.use(express.static(path.resolve(__dirname, "public")));
+
+// set path for views
+app.set("views", "src/views");
+
+//
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
