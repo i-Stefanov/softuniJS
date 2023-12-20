afc07753b48ea55268cec882f9ee6420e2cb4133
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-12-20 16:01:06 +0200

    route guards

---
+++ softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../config/env");
+
+exports.auth = (req, res, next) => {
+  // get the token from the request
+  const token = req.cookies["user"];
+  if (token) {
+    jwt.verify(token, SECRET, (err, decodedToken) => {
+      if (err) {
+        // if there is an error delete the token from cookies
+        res.clearCookie("user");
+        return next(err);
+      }
+      // attach the token to the req.user object
+      req.user = decodedToken;
+      // attach the token to the locals user object in order to be able to pass the user to the views
+      res.locals.user = decodedToken;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+// route guard middleware for unregistered users
+exports.isAuthenticated = (req, res, next) => {
+  if (!req.user) {
+    return res.redirect("/login");
+  }
+  next();
+};
+// route guard middleware for registered users
+exports.isGuest = (req, res, next) => {
+  if (req.user) {
+    return res.redirect("/");
+  }
+  next();
+};
