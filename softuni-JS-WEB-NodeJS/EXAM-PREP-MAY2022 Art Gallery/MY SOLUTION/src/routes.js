756c9d8c894e1374c8635ed00d4332123a1aa397
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-01-02 18:00:33 +0200

    add publication controller to routes

--- softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/routes.js
+++ softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController.js");
 const authController = require("./controllers/authController.js");
+const publicationController = require("./controllers/publicationController.js");
 
 // each request will load the home controller first and the the controller that matches the path given
 router.use(homeController);
 router.use(authController);
+router.use("/publications", publicationController);
 module.exports = router;
