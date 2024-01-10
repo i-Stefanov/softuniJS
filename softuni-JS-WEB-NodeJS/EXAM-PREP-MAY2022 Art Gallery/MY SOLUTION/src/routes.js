6fb0906a930de41004d0ff5a8ce708f55bede008
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-01-10 17:34:24 +0200

    add profile controller to routes

--- softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/routes.js
+++ softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/routes.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController.js");
 const authController = require("./controllers/authController.js");
 const publicationController = require("./controllers/publicationController.js");
-
+const profileController = require("./controllers/profileController.js");
 // each request will load the home controller first and the the controller that matches the path given
 router.use(homeController);
 router.use(authController);
+router.use(profileController);
 router.use("/publications", publicationController);
 module.exports = router;
