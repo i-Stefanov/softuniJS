c68f55783b0db98616279c86796f2f61c591e322
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-01-10 17:33:35 +0200

    add shares array length to publications

--- softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/controllers/homeController.js
+++ softuni-JS-WEB-NodeJS/EXAM-PREP-MAY2022 Art Gallery/MY SOLUTION/src/controllers/homeController.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const publicationService = require("../services/publicationService");
 
 router.get("/", async (req, res) => {
-  const publications = await publicationService.getAll().lean();
+  const publicationsResult = await publicationService.getAll().lean();
+  const publications = publicationsResult.map((p) => ({
+    ...p,
+    sharesCount: p.usersShared.length,
+  }));
   res.render("homeView", { publications });
 });
 
