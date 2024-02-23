9b3644de90f6e00b05774859e51a7b29fd192101
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-02-23 13:34:48 +0200

    basic nav component

--- ANGULAR/02. Angular Components/angular16-components-demo/src/app/app.module.ts
+++ ANGULAR/02. Angular Components/angular16-components-demo/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './navigation/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
