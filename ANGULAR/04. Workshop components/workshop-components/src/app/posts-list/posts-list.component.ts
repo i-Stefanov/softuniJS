f2ee9ccdf61f065184e16be29c086fb30d293394
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-03-02 21:29:34 +0200

    workshop components final commit

---
+++ ANGULAR/04. Workshop components/workshop-components/src/app/posts-list/posts-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Post } from '../types/post';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css'],
+})
+export class PostsListComponent implements OnInit {
+  posts: Post[] = [];
+  isLoading: boolean = true;
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    // this.api.getPosts(5).subscribe((posts) => {
+    //   setTimeout(() => {
+    //     this.posts = posts;
+    //     this.isLoading = false;
+    //   }, 3000);
+    //   console.log('all posts  ', posts);
+    // });
+
+    this.api.getPosts(5).subscribe({
+      next: (posts) => {
+        setTimeout(() => {
+          this.posts = posts;
+          this.isLoading = false;
+        }, 3000);
+        console.log('all posts  ', posts);
+      },
+      error: (err) => {
+        console.error('Error ', err);
+        this.isLoading = false;
+      },
+    });
+  }
+}
