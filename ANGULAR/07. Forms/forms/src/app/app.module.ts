07096ebcc859459b101f6d0ec0828ca100bba811
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-03-07 15:56:30 +0200

    custom directive similar to roterLink

--- ANGULAR/07. Forms/forms/src/app/app.module.ts
+++ ANGULAR/07. Forms/forms/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyRouterLinkDirective } from './my-router-link.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyRouterLinkDirective
   ],
   imports: [
     BrowserModule,
