9ff3308c681a071f3c7c5baa5fabfbb65cd21204
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-03-07 18:05:34 +0200

    case 3 create template variable

--- ANGULAR/07. Forms/forms/src/app/my-structural-directive.directive.ts
+++ ANGULAR/07. Forms/forms/src/app/my-structural-directive.directive.ts
@@ -10,18 +10,24 @@ import {
 
 @Directive({
   selector: '[appMyStructuralDirective]',
+  exportAs: 'MyStructuralDirectiveDirective',
 })
 export class MyStructuralDirectiveDirective implements OnChanges {
   @Input() appMyStructuralDirective: boolean = false;
+  @Input() myTmpProp: any;
   constructor(
     @Optional() private templateRef: TemplateRef<any>,
     private vcRef: ViewContainerRef
   ) {}
 
   ngOnChanges(changes: SimpleChanges): void {
+    const template = this.templateRef || this.myTmpProp;
+    if (!template) {
+      return;
+    }
     if (this.appMyStructuralDirective) {
       // shows the element if true
-      this.vcRef.createEmbeddedView(this.templateRef, {
+      this.vcRef.createEmbeddedView(template, {
         fromDirective: 'value form ngOnChanges 123',
       });
     } else {
