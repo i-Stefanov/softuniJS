82a24e3c4d1520e6ea15ebd8d71c9359bf66050e
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2024-03-07 18:43:07 +0200

    create login form with reset

--- ANGULAR/07. Forms/forms/src/app/login/login.component.ts
+++ ANGULAR/07. Forms/forms/src/app/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  submitHandler(form: NgForm): void {
+    //? form refers to the form in login
+    //? using setValue all values can b reset after submit
+    // form.setValue({
+    //   email: '',
+    //   password: '',
+    // });
 
+    if (form.invalid) {
+      return;
+    }
+    console.log(form.value);
+    const value: { email: string; password: string } = form.value;
+  }
 }
